824b9fcba967f2434ec9a778cd7ad03353f36188
Yoko Andrae <yokoandrae@Yokos-MBP.lan>
2023-05-01 17:39:43 +0200

    refactor Form component

--- src/client/components/form/Form.tsx
+++ src/client/components/form/Form.tsx
@@ -18,17 +18,17 @@ import { expenseFormMap, incomeFormMap } from '../../map/form'
 
 interface IFormItem {
     [key: string]: 
-        FunctionComponent<InputProps>
-        |
-        FunctionComponent<InputSelectProps>
+        |FunctionComponent<InputProps>
+        |FunctionComponent<InputSelectProps>
 }
+type FormValueState = {[key: string]: number | string}
 
 const formItem: IFormItem = {
     Input, 
     Select
 }
 
-const initFormValues: {[key: string]: number | string} = {
+const initFormValues: FormValueState = {
     'food': '',
     'school': '',
     'hobby': '',
@@ -37,6 +37,70 @@ const initFormValues: {[key: string]: number | string} = {
     'year': '',
     'month': '',
     'memo': ''
+}
+
+const createForm = (type: string, state: FormValueState, inputValueHandler: (...args: any) => void) => {
+    const form = type === 'expense' ? expenseFormMap : incomeFormMap
+    return form.map((item, i) => {
+        // If several components need to be in one row.
+        if ('components' in item) {
+            return (
+                <div key={i} className='flex justify-end gap-2 h-12'>
+                    {
+                        (item.components as ComponentInArray[]).map((componentRow, j) => {
+                            if (typeof formItem[componentRow.component] !== 'undefined') {
+                                const component = componentRow.component as string
+                                if (component === 'Input') {
+                                    return createElement(
+                                        formItem[component] as FunctionComponent<InputProps>,
+                                        {
+                                            ...componentRow.props,
+                                            value: state[componentRow.props.id],
+                                            onChange: inputValueHandler,
+                                            key: 'nested-' + j
+                                        }
+                                    )
+                                }
+                                return createElement(
+                                    formItem[component] as FunctionComponent<InputSelectProps>, 
+                                    {
+                                        ...componentRow.props,
+                                        onChange: inputValueHandler,
+                                        key: 'nested-' + j
+                                    }
+                                )
+                            }
+                        })
+                    }
+                </div>
+            ) 
+        }
+        // If one component per row.
+        else if (item.component && typeof formItem[item.component] !== 'undefined') {
+            if (item.component === 'Input') {             
+                return createElement(
+                    formItem[item.component] as FunctionComponent<InputProps>,
+                    {  
+                        ...item.props,
+                        value: state[item.props.id],
+                        onChange: inputValueHandler,
+                        key: i
+                    }
+                )
+            }
+            return createElement(
+                formItem[item.component] as FunctionComponent<InputSelectProps>, 
+                {   ...item.props,
+                    onChange: inputValueHandler,
+                    key: i
+                }
+            )
+        }
+        return createElement(
+            item.component!,
+            { children: item.content, key: i }
+        )
+    })
 }
 
 const Form = ({
@@ -76,73 +140,9 @@ const Form = ({
         
         return subTotal
     }
-
-    const createForm = (type: string) => {
-        const form = type === 'expense' ? expenseFormMap : incomeFormMap
-        return form.map((item, i) => {
-            // If several components need to be in one row.
-            if ('components' in item) {
-                return (
-                    <div key={i} className='flex justify-end gap-2 h-12'>
-                        {
-                            (item.components as ComponentInArray[]).map((componentRow, j) => {
-                                if (typeof formItem[componentRow.component] !== 'undefined') {
-                                    const component = componentRow.component as string
-                                    if (component === 'Input') {
-                                        return createElement(
-                                            formItem[component] as FunctionComponent<InputProps>,
-                                            {
-                                                ...componentRow.props,
-                                                value: state[componentRow.props.id],
-                                                onChange: handleInputValue,
-                                                key: 'nested-' + j
-                                            }
-                                        )
-                                    }
-                                    return createElement(
-                                        formItem[component] as FunctionComponent<InputSelectProps>, 
-                                        {
-                                            ...componentRow.props,
-                                            onChange: handleInputValue,
-                                            key: 'nested-' + j
-                                        }
-                                    )
-                                }
-                            })
-                        }
-                    </div>
-                ) 
-            }
-            // If one component per row.
-            else if (item.component && typeof formItem[item.component] !== 'undefined') {
-                if (item.component === 'Input') {             
-                    return createElement(
-                        formItem[item.component] as FunctionComponent<InputProps>,
-                        {  
-                            ...item.props,
-                            value: state[item.props.id],
-                            onChange: handleInputValue,
-                            key: i
-                        }
-                    )
-                }
-                return createElement(
-                    formItem[item.component] as FunctionComponent<InputSelectProps>, 
-                    {   ...item.props,
-                        onChange: handleInputValue,
-                        key: i
-                    }
-                )
-            }
-            return createElement(
-                item.component!,
-                { children: item.content, key: i }
-            )
-        })
-    }
-
-    const expenseForm = createForm('expense')
-    const incomeForm = createForm('income')
+    
+    const expenseForm = createForm('expense', state, handleInputValue)
+    const incomeForm = createForm('income', state, handleInputValue)
     
     return (
         <>
